3c1f116ba3490b48ae09dce029a1362f210248be
Xiao001010 <haoxu.edu@outlook.com>
2023-04-25 00:48:46 +0100

    inference

--- inference.py
+++ inference.py
@@ -10,12 +10,11 @@ import matplotlib.pyplot as plt
 import torchlm
 
 from utils import *
-from utils import Recover
 from resnet import *
 from datasets import FaceDataset, CascadeStage2Dataset
 
 arg = argparse.ArgumentParser()
-arg.add_argument('--config', type=str, default='config/inference.yaml', help='Path to config file')
+arg.add_argument('--config', type=str, default='config/Inference/Inference_Cas_Stage2_noAug_MSE-S1_noAug_MSE-lr0.5g0.9_B2.yaml', help='Path to config file')
 args = arg.parse_args()
 
 config = yaml.load(open(args.config, 'r'), Loader=yaml.FullLoader)
@@ -24,7 +23,7 @@ config = yaml.load(open(args.config, 'r'), Loader=yaml.FullLoader)
 
 # DEVICE
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-DEVICE = "cpu"
+# DEVICE = "cpu"
 print("Using device:", DEVICE)
 
 TASK = config['TASK']
@@ -41,22 +40,42 @@ MODEL_NAME = config['MODEL']['MODEL_NAME']
 NUM_OUTPUTS = config['MODEL']['NUM_OUTPUTS']
 LOAD_PATH = config['MODEL']['LOAD_PATH']
 
+# Inference hyperparameters
+RECOVER = config['INFERENCE']['RECOVER']
+PLOT_ON_RAW_DATA = config['INFERENCE']['PLOT_ON_RAW_DATA']
+STOP_IDX = config['INFERENCE']['STOP_IDX']
+SAVE_PREDICTIONS = config['INFERENCE']['SAVE_PREDICTIONS']
+PLOT = config['INFERENCE']['PLOT']
+
 if STAGE == 1:
-    print("Load dataset for cascade stage 1")
+    print("Load dataset for cascade stage 1 from {}".format(TEST_PATH))
     test_dataset = FaceDataset(path=TEST_PATH, partial=True, augment=False, inference=INFERENCE)
 elif STAGE == 2:
     print("Load stage 1 model")
     stage1_model = resnet18(pretrained=False, num_classes=10).to(DEVICE)
     load_checkpoint(torch.load(STAGE1_MODEL_PATH), stage1_model)
-    print("Load dataset for cascade stage 2")
-    test_dataset = FaceDataset(path=TEST_PATH, partial=False, augment=False, inference=INFERENCE)
+    print("Load dataset for cascade stage 2 from {}".format(TEST_PATH))
+    if INFERENCE:
+        test_dataset = CascadeStage2Dataset(path=TEST_PATH, model=stage1_model, augment=False, device=DEVICE, inference=INFERENCE)
+    else:
+        test_dataset = CascadeStage2Dataset(path=TEST_PATH, model=stage1_model, augment=False, device=DEVICE, inference=INFERENCE, test=True)
 else:
-    print("Load dataset for {}".format(TASK))
+    print("Load dataset for {} from {}".format(TASK, TEST_PATH))
     test_dataset = FaceDataset(path=TEST_PATH, partial=False, augment=False, inference=INFERENCE)
 
 
 test_loader = DataLoader(dataset=test_dataset,
                          batch_size=1, shuffle=False)
+
+raw_data = np.load(TEST_PATH)
+raw_images = raw_data['images']
+try:
+    raw_landmarks = raw_data['points']
+except:
+    print("No landmarks in raw data")
+    raw_landmarks = None
+raw_image = None
+raw_landmark = None
 
 # Initialize network
 print("Initializing network {} with {} outputs...".format(
@@ -79,16 +98,67 @@ else:
 print("Loading model from {}".format(LOAD_PATH))
 load_checkpoint(torch.load(LOAD_PATH), model)
 
-for i, (image, landmarks) in enumerate(tqdm(test_loader)):
-    image = image.to(DEVICE)
-    fake_image = torch.zeros_like(image)
-    landmarks = landmarks.reshape(-1, 2)
-    output = model(image).cpu().detach()
-    image = image.cpu().detach()
-    image, pred = Recover(image, output)
-    fake_image, landmarks = Recover(fake_image, landmarks)
-    plt.imshow(image)
-    plt.plot(pred[:, 0], pred[:, 1], 'bo')
-    plt.plot(landmarks[:, 0], landmarks[:, 1], 'ro')
-    plt.show()
-    break
+# Inference
+print("Starting inference...")
+model.eval()
+if STAGE == 1 or STAGE is None:
+    indices = (20, 29, 16, 32, 38)
+    if INFERENCE:
+        for i, (image, fake_landmarks) in enumerate(tqdm(test_loader)):
+            if PLOT_ON_RAW_DATA and RECOVER:
+                raw_image = raw_images[i]
+                if STAGE == 1:
+                    raw_landmark = raw_landmarks[i][indices, :]
+                else:
+                    raw_landmark = raw_landmarks[i]
+            image, pred = Inferencer(model, image, recover=RECOVER, landmarks=None, angle=0, 
+                       device=DEVICE, raw_image=raw_image, raw_landmark=raw_landmark, plot=PLOT)
+            if STOP_IDX and i == STOP_IDX:
+                break
+    else:
+        for i, (image, landmarks) in enumerate(tqdm(test_loader)):
+            if PLOT_ON_RAW_DATA and RECOVER:
+                raw_image = raw_images[i]
+                if STAGE == 1:
+                    raw_landmark = raw_landmarks[i][indices, :]
+                else:
+                    raw_landmark = raw_landmarks[i]
+            image, pred = Inferencer(model, image, recover=RECOVER, landmarks=landmarks, 
+                       angle=0, device=DEVICE, raw_image=raw_image, raw_landmark=raw_landmark, plot=PLOT)
+            if STOP_IDX and i == STOP_IDX:
+                break
+elif STAGE == 2:
+    if INFERENCE:
+        predictions =  []
+        for i, (image, angle) in enumerate(tqdm(test_loader)):
+            if PLOT_ON_RAW_DATA and RECOVER:
+                raw_image = raw_images[i]
+                try:
+                    raw_landmark = raw_landmarks[i]
+                except:
+                    # print("No landmarks in raw data")
+                    raw_landmark = None
+            angle = angle.item()
+            image, pred = Inferencer(model, image, recover=RECOVER, landmarks=None, 
+                       angle=angle, device=DEVICE, raw_image=raw_image, raw_landmark=raw_landmark, plot=PLOT)
+            predictions.append(pred)
+            if STOP_IDX and i == STOP_IDX:
+                break
+    else:
+        for i, (image, landmarks, angle) in enumerate(tqdm(test_loader)):
+            if PLOT_ON_RAW_DATA and RECOVER:
+                print("plot on raw data")
+                raw_image = raw_images[i]
+                raw_landmark = raw_landmarks[i]
+            angle = angle.item()
+            # print("angle", type(angle), angle)
+            # print("landmark", max(raw_landmark[:, 0]), max(raw_landmark[:, 1]))
+            image, pred = Inferencer(model, image, recover=RECOVER, landmarks=landmarks, 
+                       angle=angle, device=DEVICE, raw_image=raw_image, raw_landmark=raw_landmark, plot=PLOT)
+            if STOP_IDX and i == STOP_IDX:
+                break
+
+if SAVE_PREDICTIONS and STAGE == 2 and INFERENCE and STOP_IDX is None:
+    print("Saving predictions...")
+    predictions = np.array(predictions)
+    save_as_csv(predictions, "./results")
